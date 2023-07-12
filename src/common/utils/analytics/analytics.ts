cd6c036bd530d8fa040abef7385daa9fb1da8c7f
Kevin <kevin.five@kunstmaan.be>
2023-07-12 11:53:38 +0200

    ga

--- src/common/utils/analytics/analytics.ts
+++ src/common/utils/analytics/analytics.ts
@@ -1,6 +1,6 @@
 import { GTM_ELEMENT_ID } from '../../../config/dom.config';
 
-const GTM_ID = 'GTM-MNKLPVF';
+const GTM_ID = 'GTM-KXPQWVM';
 const GTM_SCRIPT = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
